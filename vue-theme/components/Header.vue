fe32363c19586d14d17eb5e83c5379455b475583
StephanMukete <elondostephan@gmail.com>
2019-02-10 02:25:21 -0800

    added header component

--- vue-theme/components/Header.vue
+++ vue-theme/components/Header.vue
@@ -1,10 +1,22 @@
 <template>
     <header class="header">
-        <ul>
+        <ul class="nav-items">
             <li>
                 <nuxt-link to="/">Home</nuxt-link>
+            </li>
+            <li>
                 <nuxt-link to="/blog">Blog</nuxt-link>
             </li>
         </ul>
     </header>
 </template>
+
+<style scoped>
+    .header{
+        width: 100%;
+        height:60px;
+        display: flex;
+        justify-content: center;
+        justify-items: center
+    }
+</style>
