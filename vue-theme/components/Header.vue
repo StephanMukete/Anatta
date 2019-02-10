98cf509953edf3515436650bbc7e6c907552e44c
StephanMukete <elondostephan@gmail.com>
2019-02-10 02:19:36 -0800

    added header component

---
+++ vue-theme/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <header class="header">
+        <ul>
+            <li>
+                <nuxt-link to="/">Home</nuxt-link>
+                <nuxt-link to="/blog">Blog</nuxt-link>
+            </li>
+        </ul>
+    </header>
+</template>
