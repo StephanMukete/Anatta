39b4eb1001feb332fbc2371bbdd04f56a230c16c
StephanMukete <elondostephan@gmail.com>
2019-02-14 22:55:44 -0800

    done styling

--- vue-theme/pages/blog.vue
+++ vue-theme/pages/blog.vue
@@ -2,68 +2,55 @@
   <section class="container">
     <Header/>
       <h1>{{preview.title[0].text}}</h1>
-                     <div class="onbanner">
-                <img :src="preview.image1.url" alt="blog cover photo"> 
-                <h5>{{preview.Image1_title[0].text}}</h5>
-                <h3>{{preview.Image1_subtitle[0].text}}</h3>
-                <p>{{preview.description[0].text}}</p>
-                <nuxt-link to="#">Read More</nuxt-link>
+          <div class="onbanner">
+              <img :src="preview.image1.url" alt="blog cover photo"> 
+              <h5>{{preview.Image1_title[0].text}}</h5>
+              <h3>{{preview.Image1_subtitle[0].text}}</h3>
+              <p>{{preview.description[0].text}}</p>
+              <nuxt-link to="#">Read More</nuxt-link>
          </div>
-      <div id="banner">
-      
-              <img  :src="preview.cover_image.url" alt="homepage">
-      <div class="space">
-        <!-- <font-awesome-icon :icon="['fab', 'BBC']" /> -->
-        <div class="f-icons">
-            <li><font-awesome-icon :icon="['fab', 'facebook']" /> </li>
-            <li><font-awesome-icon :icon="['fab', 'twitter']" /></li>
-        <li><font-awesome-icon :icon="['fab', 'instagram']" /></li>
-        <li> <font-awesome-icon :icon="['fab', 'youtube']" /></li>
+         <div id="banner">
+            <img  :src="preview.cover_image.url" alt="homepage">
+            <div class="space">
+                <div class="f-icons">
+                    <li><font-awesome-icon :icon="['fab', 'facebook']" /> </li>
+                    <li><font-awesome-icon :icon="['fab', 'twitter']" /></li>
+                    <li><font-awesome-icon :icon="['fab', 'instagram']" /></li>
+                    <li> <font-awesome-icon :icon="['fab', 'youtube']" /></li>
+                </div>
+            </div>
+
+            <div id="banner2" v-for="i in Math.ceil(photos.length / 2)" :key=i.id>
+                <div class="content">
+                    <form name="contact" action="" method="post">
+                        <div class="box">
+                            <h2>be updated</h2>
+                            <p>subscribe to my channel to make sure you dont miss any info</p>
+                            <label class="form-label" for="email">
+                              Email:
+                            </label>
+                            <input class="form-field" name="email" id="email" />
+                            <input class="form-button" type="submit" value="Subscribe" />
+                        </div>
+                    </form>
+                    <span class="row" v-for=" photo in photos.slice((i-1)*2, i*2)" :key=photo.id>    
+                        <img :src="photo.gallery_image.url" alt="image1">   
+                    </span>
+                </div>
+            </div>
+        
+            <div class="col3" v-for="i in Math.ceil(blogs.length / 2)" :key=i.id>
+                <span class="rowq" v-for=" blog in blogs.slice((i-1)*2, i*2)" :key=blog.id>
+                    <img :src="blog.image_banner.url" alt="image1">
+                    <h5> {{blog.title_of_banner[0].text}} </h5>
+                    <h2> {{blog.subtitle_of_banner[0].text}} </h2>
+                    <p> {{blog.description[0].text}} </p>
+                </span>
+            </div>
+        
+            <Footer/>
         </div>
-
-      </div>
-        <div id="banner2" v-for="i in Math.ceil(photos.length / 2)" :key=i.id>
-        
-            <div class="content">
-              
-      <form name="contact" action="" method="post">
-        <div class="box">
-        <h2>be updated</h2>
-        <p>subscribe to my channel to make sure you dont miss any info</p>
-        <label class="form-label" for="email">
-          Email:
-        </label>
-        <input class="form-field" name="email" id="email" />
-
-        <input class="form-button" type="submit" value="Subscribe" />
-        </div>
-
-      </form>
-    
-        
-              <span class="row" v-for=" photo in photos.slice((i-1)*2, i*2)" :key=photo.id>
-          <!-- <div  id="banner2" v-for="photo in photos" :key=photo.id> -->
-                <img :src="photo.gallery_image.url" alt="image1"> 
-              
-              </span>
-             
-           </div>
-           </div>
-                        <div class="col3" v-for="i in Math.ceil(blogs.length / 2)" :key=i.id>
-              <span class="rowq" v-for=" blog in blogs.slice((i-1)*2, i*2)" :key=blog.id>
-
-            <img :src="blog.image_banner.url" alt="image1">
-            <h5> {{blog.title_of_banner[0].text}} </h5>
-            <h2> {{blog.subtitle_of_banner[0].text}} </h2>
-            <p> {{blog.description[0].text}} </p>
-              </span>
-          </div>
-    <Footer/>
-        
-      </div>
-  
-
-  </section>
+    </section>
 </template>
 
 <script>
@@ -71,124 +58,41 @@ import Prismic from "prismic-javascript"
 import Header from '~/components/Header.vue'
 import Footer from '~/components/Footer.vue'
 
-// import Header from '~/components/Header.vue'
-
 export default {
     components: {
     Header,
     Footer
   },
   async asyncData(context) {
-  var apiEndpoint ="https://vue-theme.cdn.prismic.io/api/v2";
-  let preview = {};
-  let photos = [];
-  let blogs = [];
- 
-  const api = await Prismic.getApi(apiEndpoint);
-  const result = await api.getByUID( 
-    'blogs','blogs'
-  );
- // const result = await api.query(Prismic.Predicates.at("document.type", "blog"));
-  preview = result.data;
-  photos = preview.body[1].items;
-  blogs = preview.body[0].items;
-  console.log(blogs);
-  return {
-      preview,
-      photos,
-      blogs
-    };
- },
- data(){
-   return {
+      var apiEndpoint ="https://vue-theme.cdn.prismic.io/api/v2";
+      let preview = {};
+      let photos = [];
+      let blogs = [];
+    
+      const api = await Prismic.getApi(apiEndpoint);
+      const result = await api.getByUID( 
+        'blogs','blogs'
+      );
 
-   }
- }
-
+      preview = result.data;
+      photos = preview.body[1].items;
+      blogs = preview.body[0].items;
+      console.log(blogs);
+      return {
+          preview,
+          photos,
+          blogs
+      };
+    }
 }
 </script>
 
 <style scoped>
-
-img{
-  width:100%;
-  height: 100%;
-}
-
-
-.onbanner{
-    margin-top: -17rem;
-    height: 27rem;
-    width:18rem;
-    margin-left: 60%;
-    background: white;
-    position: absolute;
-}
- .onbanner img{
-    height: 50%;
-    width:100%;
-    /* margin-top: 20rem; */
-    /* margin-left: 20rem */
-}
-.flex{
-    display: flex;
-    float: left;
-    flex-direction: column;
-
-     /* flex-wrap: nowrap; */
-}
-/* .flex .col11{
-  width: 30%;
-  height: 100%;} */
-.f{
-      display: flex;
-      flex-direction: column;
-      
-  /* grid-template-columns: repeat(2, 1fr); */
-/* margin-top: 10rem;
-margin-left: 40rem;  */
-/* height: 10rem; */
-}
-.flex2{
-      display: flex;
-      flex-direction: column;
-      
-  /* grid-template-columns: repeat(2, 1fr); */
-margin-top: 1rem;
-margin-left: 20rem; 
-/* height: 10rem; */
+.col3{
+  margin-left:20rem
 }
 .col3 img {
-    width: 100%;
-    height: 25rem;
-}
-.flex2 h2 {
-    width: 50%;
-    height: 50%;
-}
-.flex2 p {
-    width: 50%;
-    height: 50%;
-}
-.col22 img {
-  top:0;
-    width: 90%;
-    height: 50%;
-}
-.space{
-  background-color: whitesmoke;
-    height: 10rem;
-}
-
-.col3{
-  position: relative;
-  top: 0;
-margin-left: 20rem; 
-    display: grid;
-  grid-template-columns: repeat(2, 1fr);
-}
-.bg{
-  padding: 2rem;
-  background-color: gray
+  width: 100%;
+  height: 25rem;
 }
 </style>
--- vue-theme/pages/index.vue
+++ vue-theme/pages/index.vue
@@ -1,67 +1,59 @@
 <template>
-  <section class="container">
-    <Header/>
-     <h1> {{preview.cover_text[0].text}} </h1>
-     
-    <div id = "banner">
-           <img  :src="preview.cover_photo.url" alt="homepage"> 
-          <div class="col2">
-               <img :src="preview.image1.url" alt="image1"> 
-               <div class="row2">
-                 <h4> {{preview.image1_maintext[0].text}} </h4>
-                 <p> {{preview.image1_subtext[0].text}} </p>
-               </div>
-              
+    <section class="container">
+        <Header/>
+            <h1> {{preview.cover_text[0].text}} </h1>
+            <div id = "banner">
+                <img  :src="preview.cover_photo.url" alt="homepage"> 
+                <div class="col2">
+                    <img :src="preview.image1.url" alt="image1"> 
+                    <div class="row2">
+                        <h4> {{preview.image1_maintext[0].text}} </h4>
+                        <p> {{preview.image1_subtext[0].text}} </p>
+                    </div>
+                </div>
+                <div class="col1">
+                    <h2> {{preview.highlight_text[0].text}} </h2> 
+                    <div class="icons">
+                    <img src="~/assets/icons/BBC_News.svg" alt="Kiwi standing on oval">
+                </div>
+                <div id="outerWrap">
+                    <div id="layer1">
+                        <img :src="preview.book_image.url" alt="image1">
+                    </div>
+                    <div id="layer2">
+                        <h3> {{preview.book_title[0].text}} </h3> 
+                        <p> {{preview.book_description[0].text}} </p>
+                    </div>
+                </div>
+                <div id="quote">
+                    <div class="quote" v-for="quote in quotes" :key=quote.id>
+                        <p>{{ quote.quote[0].text }}</p>
+                        <div class="col2">
+                            <img :src="quote.portrait_author.url" alt="image1">
+                            <p>{{ quote.name_of_the_author[0].text }}</p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                <div class="col2">
+                    <img :src="preview.blog1_image.url" alt="image1"> 
+                    <div class="row2">
+                        <h4> {{preview.blog_highlight[0].text}} </h4>
+                        <h2> {{preview.blog1_tittle[0].text}} </h2>
+                        <p> {{preview.blog1_description[0].text}} </p> 
+                    </div>
+                </div>
+                <div class="col3" v-for="i in Math.ceil(blogs.length / 3)" :key=i.id>
+                    <span class="rowq" v-for=" blog in blogs.slice((i-1)*3, i*3)" :key=blog.id>
+                        <img :src="blog.image_banner.url" alt="image1">
+                        <h2> {{blog.title_of_banner[0].text}} </h2>
+                        <p> {{blog.description[0].text}} </p>
+                    </span>
+                </div>
+                
+                <Footer />
           </div>
-          <div class="col1">
-         
-             <h2> {{preview.highlight_text[0].text}} </h2> 
-                <div class="icons">
-              <img src="~/assets/icons/BBC_News.svg" alt="Kiwi standing on oval">
-            </div>
-            <div id="outerWrap">
-              <div id="layer1">
-                <img :src="preview.book_image.url" alt="image1">
-              </div>
-              <div id="layer2">
-                              <h3> {{preview.book_title[0].text}} </h3> 
-              <p> {{preview.book_description[0].text}} </p>
-              </div>
-            </div>
-            <div id="quote">
-              <div class="quote" v-for="quote in quotes" :key=quote.id>
-                 <p>{{ quote.quote[0].text }}</p>
-                 <div class="col2">
-                      <img :src="quote.portrait_author.url" alt="image1">
-                 <p>{{ quote.name_of_the_author[0].text }}</p>
-                 </div>
-              
-              </div>
-            </div>
-
-          </div>
-          <div class="col2">
-           <img :src="preview.blog1_image.url" alt="image1"> 
-            <div class="row2">
-                <h4> {{preview.blog_highlight[0].text}} </h4>
-                <h2> {{preview.blog1_tittle[0].text}} </h2>
-                <p> {{preview.blog1_description[0].text}} </p> 
-            </div>
-          </div>
-           <div class="col3" v-for="i in Math.ceil(blogs.length / 3)" :key=i.id>
-              <span class="rowq" v-for=" blog in blogs.slice((i-1)*3, i*3)" :key=blog.id>
-            <img :src="blog.image_banner.url" alt="image1">
-            <h2> {{blog.title_of_banner[0].text}} </h2>
-            <p> {{blog.description[0].text}} </p>
-          </span>
-          </div>
-          <Footer />
-     
-    </div>
-    <div class="col2">
-        <!-- <img :src="preview.image1.url" alt="image1">  -->
-    </div>
-  </section>
+      </section>
 </template>
 
 <script>
