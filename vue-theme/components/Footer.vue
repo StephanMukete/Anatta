ea33f78ca8e7e6950cebaa26b92f1e12a30e86a4
StephanMukete <elondostephan@gmail.com>
2019-02-15 21:57:41 -0800

    edited header

--- vue-theme/components/Footer.vue
+++ vue-theme/components/Footer.vue
@@ -9,13 +9,12 @@
             </li>
             <li><font-awesome-icon :icon="['fab', 'facebook']" /> </li>
             <li><font-awesome-icon :icon="['fab', 'twitter']" /></li>
-        <li><font-awesome-icon :icon="['fab', 'instagram']" /></li>
-        <li> <font-awesome-icon :icon="['fab', 'youtube']" /></li>
+            <li><font-awesome-icon :icon="['fab', 'instagram']" /></li>
+            <li> <font-awesome-icon :icon="['fab', 'youtube']" /></li>
         </ul>
          <div class="copyright">
              ©2019 Dr Chattergee All rights reserved 
         </div>
-    </footer>
-     
+    </footer>   
 </template>
       
--- vue-theme/components/Header.vue
+++ vue-theme/components/Header.vue
@@ -12,22 +12,4 @@
 </template>
 
 <style scoped>
-    /* .header{
-        width: 100%;
-        height:60px;
-        display: flex;
-        justify-content: right;
-        float: right;
-        justify-items: center;
-    }
-
-    .nav-items{
-        list-style-type: none;
-        padding: 0%;
-        margin: 40px;
-        display: flex;
-        justify-content: space-around;
-        float: right;
-        width: 20%;
-    } */
 </style>
