7ad3671ba71905c73ceca825a4a4e96b9bcb425d
StephanMukete <elondostephan@gmail.com>
2019-02-21 20:14:28 -0800

    added colors to social media icons

--- vue-theme/components/Icon.vue
+++ vue-theme/components/Icon.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="f-icons">
-        <li><font-awesome-icon :icon="['fab', 'facebook']" /> </li>
-        <li><font-awesome-icon :icon="['fab', 'twitter']" /></li>
-        <li><font-awesome-icon :icon="['fab', 'instagram']" /></li>
-        <li> <font-awesome-icon :icon="['fab', 'youtube']" /></li>
+        <li><font-awesome-icon class="facebook" :icon="['fab', 'facebook']" /> </li>
+        <li><font-awesome-icon class="twitter" :icon="['fab', 'twitter']" /></li>
+        <li><font-awesome-icon class="instagram" :icon="['fab', 'instagram']" /></li>
+        <li> <font-awesome-icon class="youtube" :icon="['fab', 'youtube']" /></li>
     </div>
 </template>
